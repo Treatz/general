8e1861a3dea030dc997cd0f3ee8b427da73ec4a9
Corey White <adventmagic@gmail.com>
2017-04-03 12:27:39 -0400

    get

--- command.py
+++ command.py
@@ -234,14 +234,49 @@ class OverSay(general.CmdSay):
             self.caller.msg("You can't do that while unconscious")
 
 class OverGet(general.CmdGet):
-    key = "get"
-    lock = "cmd:all()"
-    def func(self):
-        if(self.caller.db.conscious == 1):
-                super(OverGet, self).func()
-        else:
-            self.caller.msg("You can't do that while unconscious")
+    """
+    pick up something
 
+    Usage:
+      get <obj>
+
+    Picks up an object from your location and puts it in
+    your inventory.
+    """
+    key = "get"
+    aliases = "grab"
+    locks = "cmd:all()"
+    arg_regex = r"\s|$"
+
+    def func(self):
+        """implements the command."""
+
+        caller = self.caller
+
+        if not self.args:
+            caller.msg("Get what?")
+            return
+        obj = caller.search(self.args, location=caller.location)
+        if not obj:
+            return
+        if caller == obj:
+            caller.msg("You can't get yourself.")
+            return
+        if not obj.access(caller, 'get'):
+            if obj.db.get_err_msg:
+                caller.msg(obj.db.get_err_msg)
+            else:
+                caller.msg("You can't get that.")
+            return
+
+        obj.move_to(caller, quiet=True)
+        caller.msg("(TEST) You pick up %s." % obj.name)
+        caller.location.msg_contents("%s picks up %s." %
+                                     (caller.name,
+                                      obj.name),
+                                     exclude=caller)
+        # calling hook method
+        obj.at_get(caller)
 
 class OverDrop(general.CmdDrop):
     key = "drop"
